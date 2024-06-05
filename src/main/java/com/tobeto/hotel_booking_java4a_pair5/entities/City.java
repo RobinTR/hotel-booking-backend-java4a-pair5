2744d32a3aa43adf4695ef18d87c0cf47e28d435
Mem Robin Çen <memrobincen@gmail.com>
2024-06-05 15:51:45 +0300

    fix files

--- src/main/java/com/tobeto/hotel_booking_java4a_pair5/entities/City.java
+++ src/main/java/com/tobeto/hotel_booking_java4a_pair5/entities/City.java
@@ -1,9 +1,7 @@
 package com.tobeto.hotel_booking_java4a_pair5.entities;
 
 import com.tobeto.hotel_booking_java4a_pair5.core.entities.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
