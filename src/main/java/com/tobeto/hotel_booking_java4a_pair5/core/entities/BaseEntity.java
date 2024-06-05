2f636521917061307499018d1095a2427cb1f068
asoylu35 <aslisoylu738@gmail.com>
2024-06-05 15:21:27 +0300

    home

--- src/main/java/com/tobeto/hotel_booking_java4a_pair5/core/entities/BaseEntity.java
+++ src/main/java/com/tobeto/hotel_booking_java4a_pair5/core/entities/BaseEntity.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
 import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
--- src/main/java/com/tobeto/hotel_booking_java4a_pair5/entities/Hotel.java
+++ src/main/java/com/tobeto/hotel_booking_java4a_pair5/entities/Hotel.java
@@ -1,8 +1,13 @@
 package com.tobeto.hotel_booking_java4a_pair5.entities;
 
 import com.tobeto.hotel_booking_java4a_pair5.core.entities.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name = "hotels")
 @Getter
