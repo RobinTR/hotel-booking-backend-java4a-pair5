e2e4bf39c6c6f895a4f8455595d3330ab0636316
Mem Robin Çen <memrobincen@gmail.com>
2024-06-07 15:18:30 +0300

    Fix hotel commit.

---
+++ src/main/java/com/tobeto/hotel_booking_java4a_pair5/services/dtos/responses/hotel/GetAllHotelResponse.java
@@ -0,0 +1,20 @@
+package com.tobeto.hotel_booking_java4a_pair5.services.dtos.responses.hotel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetAllHotelResponse {
+    private Integer id;
+    private String addressName;
+    private String hotelReviewDescription;
+    private String name;
+    private String contactNumber;
+    private String email;
+    private String website;
+    private String description;
+    private String floorCount;
+    private String roomCapacity;
+    private int starRating;
+}
