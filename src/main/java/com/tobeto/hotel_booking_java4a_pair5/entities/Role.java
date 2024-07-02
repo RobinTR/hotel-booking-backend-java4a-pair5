0af149d3f4a02cd67deb0ebbf901b208c558615d
Mem Robin Çen <memrobincen@gmail.com>
2024-07-02 23:36:47 +0300

    added roles to jwt

--- src/main/java/com/tobeto/hotel_booking_java4a_pair5/entities/Role.java
+++ src/main/java/com/tobeto/hotel_booking_java4a_pair5/entities/Role.java
@@ -5,7 +5,8 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
     GUEST,
     ADMIN,
-    MANAGER;
+    MANAGER,
+    USER;
 
     @Override
     public String getAuthority() {
