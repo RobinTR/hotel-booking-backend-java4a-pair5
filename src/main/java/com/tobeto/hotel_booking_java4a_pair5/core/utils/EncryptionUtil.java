b49b62ec028d8f2d4c0cd62abca1a8adf0cacb71
Mem Robin Çen <memrobincen@gmail.com>
2024-07-16 14:50:27 +0300

    Refactor encryption

--- src/main/java/com/tobeto/hotel_booking_java4a_pair5/core/utils/EncryptionUtil.java
+++ src/main/java/com/tobeto/hotel_booking_java4a_pair5/core/utils/EncryptionUtil.java
@@ -1,10 +1,8 @@
 package com.tobeto.hotel_booking_java4a_pair5.core.utils;
 
-import com.tobeto.hotel_booking_java4a_pair5.core.utils.exceptions.types.BusinessException;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
@@ -12,35 +10,27 @@ public class EncryptionUtil {
     private static final String AES = "AES";
     private static final String AES_TRANSFORMATION = "AES/CBC/PKCS5Padding";
 
-    public static String encryptCardNumber(String cardNumber, SecretKey secretKey) {
+    public static String encryptCardNumber(String cardNumber, SecretKey secretKey) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         return encrypt(cardNumber, secretKey);
     }
 
-    public static String encryptCvv(String cvv, SecretKey secretKey) {
+    public static String encryptCvv(String cvv, SecretKey secretKey) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         return encrypt(cvv, secretKey);
     }
 
-    public static String encrypt(String value, SecretKey secretKey) {
-        try {
-            Cipher cipher = Cipher.getInstance(AES_TRANSFORMATION);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] encryptedData = cipher.doFinal(value.getBytes());
+    public static String encrypt(String value, SecretKey secretKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(AES_TRANSFORMATION);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(value.getBytes());
 
-            return Base64.getEncoder().encodeToString(encryptedData);
-        } catch (Exception e) {
-            throw new BusinessException("Encryption error: " + e.getMessage());
-        }
+        return Base64.getEncoder().encodeToString(encryptedData);
     }
 
-    public static SecretKey generateAESKey() {
-        try {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
-            SecureRandom secureRandom = new SecureRandom();
-            keyGenerator.init(256, secureRandom);
+    public static SecretKey generateAESKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        SecureRandom secureRandom = new SecureRandom();
+        keyGenerator.init(256, secureRandom);
 
-            return keyGenerator.generateKey();
-        } catch (Exception e) {
-            throw new BusinessException("AES key generation error: " + e.getMessage());
-        }
+        return keyGenerator.generateKey();
     }
 }
